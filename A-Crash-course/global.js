d181513607b248b798bf0bdba2f5641b1cf93939
Omosh Feloh <omondifelixotieno2@gmail.com>
2025-02-01 18:24:38 +0300

    initial commit: node.js basics

---
+++ A-Crash-course/global.js
@@ -0,0 +1,16 @@
+// Global Object
+
+// console.log(global);
+
+/* setTimeout(() => {
+    console.log("This is the timeout");
+    clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+    console.log("This is the interval");
+}, 1000);
+*/
+
+console.log(__dirname); // Absolute path of the current folder
+console.log(__filename); // Absolute path of the current file
---
+++ A-Crash-course/test.js
@@ -0,0 +1,6 @@
+const greeting = (name) => {
+    console.log(`Hello my name is ${name}`);
+}
+
+greeting("Marcus");
+greeting("Alice");

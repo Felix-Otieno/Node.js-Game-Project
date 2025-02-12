37aeb76a0d82679a11d6ab56453e30042208654c
Omosh Feloh <omondifelixotieno2@gmail.com>
2025-02-12 21:40:40 +0300

    network request updates

---
+++ Fetch API/app.js
@@ -0,0 +1,22 @@
+const getUsers = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const response = await fetch(url);
+    return await response.json();
+  };
+  
+  const render = (users) => {
+    return users.map(({ name, email }) => `<li>${name} (${email})</li>`).join('');
+  };
+  
+  (async () => {
+    try {
+      // fetch the users
+      const users = await getUsers();
+  
+      // show the user list
+      document.querySelector('#content').innerHTML = `<ul>${render(users)}</ul>`;
+    } catch (err) {
+      // show the error message
+      document.querySelector('#message').textContent = err.message;
+    }
+  })();
